39ffe4b094816441d06264d277cc679ddd805b7a
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-10-26 23:35:58 +0300

    fix: don't change perms

--- misc/deploy-runner.sh
+++ misc/deploy-runner.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-chmod 666 "${GITHUB_WORKSPACE}/misc/sber"
+# chmod 666 "${GITHUB_WORKSPACE}/misc/sber"
 rsync -azvc -e "ssh -i ${GITHUB_WORKSPACE}/misc/sber" --delete "${GITHUB_WORKSPACE}/build/" root@178.170.192.146:/home/docker/hack/frontend
