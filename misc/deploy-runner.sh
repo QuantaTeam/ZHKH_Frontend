3a96697726e5f9055eede87191a628589e79e050
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-10-26 23:38:22 +0300

    fix: change permissions still

--- misc/deploy-runner.sh
+++ misc/deploy-runner.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-# chmod 666 "${GITHUB_WORKSPACE}/misc/sber"
+chmod 600 "${GITHUB_WORKSPACE}/misc/sber"
 rsync -azvc -e "ssh -i ${GITHUB_WORKSPACE}/misc/sber -o StrictHostKeyChecking=no" --delete "${GITHUB_WORKSPACE}/build/" root@178.170.192.146:/home/docker/hack/frontend
