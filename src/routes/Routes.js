e006d21eaf3f10a6227d5d7a5302b142dd50743a
kmedvedev <you@example.com>
2022-10-26 11:49:43 +0300

    init

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import { HomePageContainer } from '../pages/HomePage/containers';
+
 import { PrivateRoute } from './PrivateRoute';
 import { ROUTES } from './routesName';
 
 export const Itinerary = () => {
   return (
     <Routes >
+      <Route path={ROUTES.HOME_PAGE} element={<HomePageContainer />} />
+      <Route path={'*'} element={<PrivateRoute> <HomePageContainer /></PrivateRoute>} />
     </Routes >
   );
 };
--- src/routes/routesName.js
+++ src/routes/routesName.js
@@ -1,13 +1,3 @@
 export const ROUTES = {
-  SIGN_IN_PAGE: '/signIn',
-  KANBAN_PAGE: '/',
-  KANBAN_PAGE_CARD: '/story/:card_id',
-  ANKETA_PAGE: 'anketa',
-  ANKETA_QR_PAGE: 'anketa/meta_from=:id',
-  ANKETA_SMS_PAGE: 'candidates/form/:id',
-  TEST_PRIVATE_PAGE: '/tests',
-  SHOW_PASSING_TEST_PAGE: '/tests/show-test/:id',
-  TEST_PUBLIC_PAGE: '/candidates/quiz/:id',
-  REFUSAL_PAGE: '/candidate-reject/:id',
-  CANDIDATE_STATUS: '/status/:id',
+  HOME_PAGE: '/',
 };
